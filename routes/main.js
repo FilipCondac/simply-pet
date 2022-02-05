130e617c67bab120de4055678a6aeed63cef06a4
filipcondac <filipcondac@gmail.com>
2022-02-05 10:54:51 +0000

    DISABLED THE FILE WHILE BUILDING WEBSITE

--- routes/main.js
+++ routes/main.js
@@ -1,11 +1,11 @@
-// Route handler for forum web app
+// // Route handler for forum web app
 
-module.exports = function (app, forumData) {
+// module.exports = function (app, forumData) {
 
-    // Handle our routes
+//     // Handle our routes
 
-    // Home page
-    app.get('/', function (req, res) {
-        res.render('index.ejs', forumData)
-    });
-}
+//     // Home page
+//     app.get('/', function (req, res) {
+//         res.render('index.ejs', forumData)
+//     });
+// }
