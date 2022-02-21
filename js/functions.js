17041bc854ae339d78c79a3d173229eeb434cdea
Zofia Kaska <zkask001@gold.ac.uk>
2022-02-21 13:54:25 +0000

    Pet passport page updated, with multiple pets

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,17 @@
 function toggleMobileMenu(menu) {
   menu.classList.toggle('open');
 }
+
+var divs = ["section1", "section2", "section3"]
+
+function show(id) {
+  for(var i = 0; i < divs.length; i++){
+    if(id == divs[i]){
+      document.getElementById(divs[i]).style.display="block"
+      document.getElementById(divs[i]).style.height="400px"
+    }else{
+      document.getElementById(divs[i]).style.display="none"
+    }
+  }
+  }
+
