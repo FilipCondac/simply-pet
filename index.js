23f00eafe249983f6c89e2bd8b00e5bac9f63c7f
filipcondac <filipcondac@gmail.com>
2022-04-01 10:50:13 +0100

    Fixed port

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ global.db = db;
 
 // Create the express application object
 const app = express()
-const port = process.env.PORT;
+const port = process.env.PORT || 8000;
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.json()) 
 
